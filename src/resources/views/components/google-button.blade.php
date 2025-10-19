8bc3134aeeef90c21995505128be69c2247f44bf
Firman Hidayat <hideyatsuapp@gmail.com>
2025-10-19 22:15:54 +0700

    feat: Update GitHub and Google button styles for consistency

--- src/resources/views/components/google-button.blade.php
+++ src/resources/views/components/google-button.blade.php
@@ -1,4 +1,4 @@
-<a href="{{ route('auth.google') }}" {{ $attributes->merge(['class' => 'btn btn-danger w-100']) }}>
+<a href="{{ route('auth.google') }}" {{ $attributes->merge(['class' => 'btn btn-google w-100']) }}>
     <x-icon name="brand-google" class="me-2" />
     @if($slot->isEmpty())
         {{ __('Login with Google') }}
